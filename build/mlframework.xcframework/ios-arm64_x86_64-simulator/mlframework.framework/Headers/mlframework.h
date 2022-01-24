b75810dc66e7abf0e401f33af8f78bd6ecd32d34
aplyamukesh <mukesh.neosoft@apalya.com>
2022-01-24 16:09:46 +0530

    Intial setup

---
+++ build/mlframework.xcframework/ios-arm64_x86_64-simulator/mlframework.framework/Headers/mlframework.h
@@ -0,0 +1,18 @@
+//
+//  mlframework.h
+//  mlframework
+//
+//  Created by webwerks on 24/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mlframework.
+FOUNDATION_EXPORT double mlframeworkVersionNumber;
+
+//! Project version string for mlframework.
+FOUNDATION_EXPORT const unsigned char mlframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mlframework/PublicHeader.h>
+
+
